a9d333bec1d91ec9bde974012057e48de7075224
Banbarashik <64987973+Banbarashik@users.noreply.github.com>
2022-01-08 04:15:49 +0700

    basic vsCPU functionality

--- script.js
+++ script.js
@@ -22,30 +22,73 @@ const winCombinations = [
   [3, 5, 7],
 ];
 
+let currentPlayer = playerX,
+  playing = true;
+
 const makeTurn = function (e) {
-  e.target.classList.add(`${currentPlayer.name}`);
+  e.target.classList.add(`${currentPlayer.name}`, 'checked');
+
   currentPlayer.positions.push(+e.target.dataset.position);
+
+  // disable the cell after it's checked
+  e.target.removeEventListener('click', makeTurn);
 
   checkWin();
 
-  currentPlayer = currentPlayer.name === 'playerX' ? playerO : playerX;
+  if (playing) {
+    currentPlayer = playerO;
+    computerTurn();
+  }
+};
+
+const computerTurn = function () {
+  let cell, randomPosition;
+  const chooseRandomCell = function () {
+    randomPosition = Math.floor(Math.random() * 9) + 1;
+
+    cell = document.querySelector(
+      `.game--cell[data-position='${randomPosition}']`
+    );
+
+    if (cell.classList.contains('checked') && playing) chooseRandomCell();
+  };
+  chooseRandomCell();
+
+  cell.classList.add(`${currentPlayer.name}`, 'checked');
+
+  currentPlayer.positions.push(randomPosition);
+
+  // disable the cell after it's checked
+  cell.removeEventListener('click', makeTurn);
+
+  checkWin();
+
+  currentPlayer = playerX;
 };
 
 const checkWin = function () {
-  // winCombinations.forEach(combination =>
-  //   combination.every(num => currentPlayer.positions.includes(num))
-  // );
+  console.log(playerX.positions);
+  console.log(playerO.positions);
   for (const winCombination of winCombinations) {
     if (winCombination.every(num => currentPlayer.positions.includes(num))) {
       alert(`${currentPlayer.name} won!`);
 
-      cells.forEach(cell => cell.removeEventListener('click', makeTurnы));
+      // disable all cells after the game end
+      cells.forEach(cell => cell.removeEventListener('click', makeTurn));
+
+      playing = false;
 
       break;
     }
   }
+  if (
+    Array.from(cells).every(cell => cell.classList.contains('checked')) &&
+    playing
+  ) {
+    alert("It's a draw!");
+
+    playing = false;
+  }
 };
 
-let currentPlayer = playerX;
-
-cells.forEach(cell => cell.addEventListener('click', makeTurn, { once: true }));
+cells.forEach(cell => cell.addEventListener('click', makeTurn));
